0059c0b2ce6672dc8d126a11457189876983bed7
Gonzalo Ramos <garamos@garamos.apn>
2026-08-12 20:49:18 -0300

    Agrega skeleton Tab de archivos siapn

---
+++ skeleton/siapn/tab/FwFileTab.php
@@ -0,0 +1,8 @@
+<?php
+########################################################################
+#   ADVERTENCIA: Si estas leyendo esto es que estas usando el skeleton
+#   tab/FwFileTab.php y no fue revisado ni modificado.
+######################################################################## 
+
+//                          table       col         perm
+$this->addTab(new FwFileTab('XXX.XXX', 'XXX_XXX'), 'XXX_XXX_W');
